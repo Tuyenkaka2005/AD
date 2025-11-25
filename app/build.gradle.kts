66a92f6d3f30ea2444761b064689f7a42eff947e
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2025-11-25 20:41:51 +0700

    First commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ad"
+    namespace = "com.budgetwise.ad"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.ad"
+        applicationId = "com.budgetwise.ad"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
